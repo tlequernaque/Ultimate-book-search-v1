fd31c1bb5c9afaee6280adae28fdc1418b082e73
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-23 06:51:53 -0400

    updated resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -53,5 +53,18 @@ const resolvers = {
             throw new AuthenticationError('You need to be logged in!');
         },
     
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+              const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId } } },
+                { new: true }
+              );
+      
+              return updatedUser;
+            }
+      
+            throw new AuthenticationError('You need to be logged in!');
+          },
     }
 }
