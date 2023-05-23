b06ba40ee9a83a6ceb810d58d46470bbb14e1eb9
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-23 06:57:47 -0400

    updated typedefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -35,4 +35,12 @@ const typeDefs = gql`
   type Query {
     me: User
   }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+
 `;
