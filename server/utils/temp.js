f837694d8f53847dda09eb4751476d68ddd8d9ca
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-23 06:25:05 -0400

    changed auth.js

--- server/utils/temp.js
+++ server/utils/temp.js
@@ -1,13 +1,14 @@
 const jwt = require('jsonwebtoken');
 
-const secret = 'mysecret';
-const expiration = '1h';
+const secret = 'mysecretsshhhhh';
+const expiration = '2h';
 
 module.exports = {
   authMiddleware: function ({ req }) {
-   
+    // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
 
+    // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
       token = token.split(' ').pop().trim();
     }
@@ -20,7 +21,7 @@ module.exports = {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
     } catch {
-      console.log('Invalid JWT token');
+      console.log('Invalid token');
     }
 
     return req;
