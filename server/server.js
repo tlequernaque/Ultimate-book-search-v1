58f8aa4ed25a2f7ff96e524d75cc6499de62b7e9
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-23 06:28:47 -0400

    updated server.js

--- server/server.js
+++ server/server.js
@@ -16,6 +16,17 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  server.applyMiddleware({ app });
+  
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+    })
+  })
+  };
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
