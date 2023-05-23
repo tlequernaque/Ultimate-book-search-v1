5020b28fb09ea10d82cd5bd733e3da01f75447cd
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-23 06:50:52 -0400

    updated resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -14,5 +14,44 @@ const resolvers = {
         throw new AuthenticationError('Not logged in');
       },
     },
-  
-  }
+
+    Mutation: {
+        addUser: async (parent, args) => {
+          const user = await User.create(args);
+          const token = signToken(user);
+    
+          return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+          const user = await User.findOne({ email });
+    
+          if (!user) {
+            throw new AuthenticationError('Incorrect credentials');
+          }
+    
+          const correctPw = await user.isCorrectPassword(password);
+    
+          if (!correctPw) {
+            throw new AuthenticationError('Incorrect credentials');
+          }
+    
+          const token = signToken(user);
+          return { token, user };
+        },
+
+        saveBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+              const updatedUser = await User.findByIdAndUpdate(
+                { _id: context.user._id },
+                { $push: { savedBooks: bookData } },
+                { new: true }
+              );
+      
+              return updatedUser;
+            }
+        
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    
+    }
+}
