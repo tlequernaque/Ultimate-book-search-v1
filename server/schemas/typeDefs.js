34d6dc75236f4c7053df5c151ab80c551fb33832
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-23 06:54:19 -0400

    updated typedefs.js

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,12 @@
 const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+`;
