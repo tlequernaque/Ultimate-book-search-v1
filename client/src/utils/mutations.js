68328fae6e726a9d424e6ef72098c5e9c0ab359c
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-05-24 01:41:46 -0400

    added REMOVE_BOOK to mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -62,3 +62,22 @@ export const SAVE_BOOK = gql`
         }
     }
 `;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: ID!) {
+        saveBook(bookId: $bookId) {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                bookId
+                authors
+                image
+                description
+                title
+                link
+            }
+        }
+    }
+`;
